bc9d92311f3934b9f1060d291f449fe980a4d93e
Leon <leonhuo@outlook.com>
2023-12-29 22:31:04 +0800

    Update main.blocks, main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# ...
+hand = 0
+
+def on_gesture_shake():
+    global hand
+    hand = randint(1, 3)
+    if hand == 1:
+        basic.show_leds("""
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            """)
+    elif hand == 2:
+        basic.show_icon(IconNames.SQUARE)
+    else:
+        basic.show_icon(IconNames.SCISSORS)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+input.is_gesture(Gesture.SHAKE)
